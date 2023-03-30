066a94ff997187a9a8ef2d4175c1010d19e65633
242425 <242425@edu.p.lodz.pl>
2023-03-30 16:21:20 +0200

    testuj to ty xd

--- Modul/src/main/java/org/example/krypto/Keys.java
+++ Modul/src/main/java/org/example/krypto/Keys.java
@@ -12,12 +12,20 @@ public class Keys {
     private final Random random = new Random();
 
     public void generateKeys() {
-        int keyLenght = 2048;
-        N = BigInteger.probablePrime(keyLenght + 2, random);
-        a = new BigInteger(keyLenght,random); //klucz prywatny
-        g = new BigInteger(keyLenght,random);
+        int keyLenght = 2046;
+        N = BigInteger.probablePrime(keyLenght + 4, random);
+//        a = new BigInteger(keyLenght, random); //klucz prywatny
+//        g = new BigInteger(keyLenght, random);
+        a = BigInteger.probablePrime(keyLenght + 2, random);
+        g = BigInteger.probablePrime(keyLenght + 2, random);
         h = g.modPow(a,N);
         Nn1 = N.subtract(BigInteger.ONE);
+//        System.out.println(N.bitLength());
+//        System.out.println(a.bitLength());
+//        System.out.println(g.bitLength());
+//        System.out.println(h.bitLength());
+
+
     }
 
     public BigInteger getN() {
